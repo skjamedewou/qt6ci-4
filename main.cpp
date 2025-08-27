ca856cd41570a915a03415b60b160adfa6f43152
SergeA <amedewous@gmail.com>
2025-08-27 18:45:05 +0200

    making tree

--- main.cpp
+++ main.cpp
@@ -1,5 +1,46 @@
 #include <QCoreApplication>
+#include "test.h"
 
+Test* makeTree()
+{
+    Test* root = new Test(nullptr, "Root");
+
+    for(int p = 0; p<5;p++)
+    {
+        Test* pobj = new Test(root, "Parent-" + QString::number(p));
+
+        for (int c = 0; c< 3;c++)
+        {
+            Test* cobj = new Test(pobj, "Child-" + QString::number(p) +" " + QString::number(c));
+
+            for (int s = 0; s< 3;s++)
+            {
+                Test* sobj = new Test(cobj, "Sub-" + QString::number(p) +" " + QString::number(c)+" " + QString::number(s));
+
+            }
+
+        }
+    }
+    return root;
+}
+
+void printTree(Test* root, int level =0)
+{
+    if (root ->children().length() ==0 ) return;
+
+    QString lead = "-";
+    lead.fill('-', level *5);
+
+    foreach(QObject* obj, root->children())
+    {
+        Test* child = qobject_cast<Test*>(obj);
+        if(!child) return;
+
+        qInfo() << lead << child ;
+
+        printTree(child, level +1);
+    }
+}
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
@@ -14,6 +55,10 @@ int main(int argc, char *argv[])
 
     // If you do not need a running Qt event loop, remove the call
     // to a.exec() or use the Non-Qt Plain C++ Application template.
+
+    Test* tree = makeTree();
+    printTree(tree, 1);
+    delete tree;
 
     return a.exec();
 }
---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include "test.h"
+
+Test::Test(QObject *parent, QString name)
+    : QObject{parent}
+{
+    setObjectName(name);
+    qInfo() << "Constructed" <<this;
+}
+
+
+Test::~Test()
+{
+ qInfo() << "DeConstructed" <<this;
+}
+
